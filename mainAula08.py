bbc9fd22049879e4601769f52e4538858bd499b0
Cassiano-code <71021277+Cassiano-code@users.noreply.github.com>
2020-10-21 10:51:17 -0300

    Add files via upload

---
+++ mainAula08.py
@@ -0,0 +1,14 @@
+from classes import Aluno
+from classes import AlunoEnsinoMedio
+from classes import AlunoGraduacao
+
+a = Aluno("123","Roberto Carlos", "98765432")
+b = AlunoEnsinoMedio("321","Carlos Roberto", "7777777","primeiro ano")
+c = AlunoGraduacao("456","Miranda","37482929","Terceiro semestre")
+
+
+a.imprimir()
+print(" ")
+b.imprimir()
+print(" ")
+c.imprimir()
